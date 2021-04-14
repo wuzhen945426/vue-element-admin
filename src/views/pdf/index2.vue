9e64e2b95adc13cb905adc91db4cf2e7627e0950
wuzhen <1090163645@qq.com>
2021-04-14 14:24:11 +0800

    PDF模块封装

---
+++ src/views/pdf/index2.vue
@@ -0,0 +1,22 @@
+<template>
+      <el-button @click="print">
+        Click to download PDF
+      </el-button>
+</template>
+
+<script>
+
+
+import printHtml from "@/utils/print.js"
+ 
+export default {
+  methods: {
+    print() {
+      let printData = `<div>打印内容1111111</div>`
+      printHtml(printData)
+    }
+  },
+}
+
+
+</script>
